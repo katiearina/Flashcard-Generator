c0cb9d1cb2eb1dfa58566dc02680ec108a5c89a4
Katie <katiearina@gmail.com>
2017-04-29 21:26:41 -0400

    :seedling: :new_moon_with_face: Well I got one card working, now just need to set it up as a recursive function.. Too tired (and hot) right now to keep working on it.

--- assets/javascript/BasicCard.js
+++ assets/javascript/BasicCard.js
@@ -14,13 +14,31 @@ BasicCard.prototype.printCard = function() {
 	console.log("?: " + this.front);
 };
 
-var 
+var question1 = new BasicCard("You speak an infinite deal of nothing.", "The Merchant of Venice");
+var question2 = new BasicCard("Sweets to the sweet.", "Hamlet");
+var question3 = new BasicCard("Better a witty fool, than a foolish wit.", "Twelfth Night");
+var question4 = new BasicCard("Some Cupid kills with arrows, some with traps.", "Much Ado About Nothing");
+var question5 = new BasicCard("Educated men are so impressive!", "Romeo and Juliet");
+var question6 = new BasicCard("By the pricking of my thumbs, something wicked this way comes.", "Macbeth");
+var question7 = new BasicCard("There's meaning in thy snores.", "The Tempest");
+var question8 = new BasicCard("If I be waspish, best beware my sting.", "The Taming of the Shrew");
 
+function playCards() {
+inquirer.prompt([
+	{
+		name: "question1",
+		message: question1.front
+	},
 
-inquirer.prompt([/* Pass your questions in here */]).then(function (answers) {
-    // Use user feedback for... whatever!! 
+	]).then(function(user) {
+		if (user.question1.toUpperCase() === question1.back.toUpperCase()) {
+			console.log("You got it right!");
+		}
+		else {
+			console.log("The correct answer was: " + question1.back.toUpperCase());
+		}
 });
-
+};
 
 
 //---------------------------------------------------------------------------
