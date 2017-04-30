3bb96d262d75e20b801fba680d84c23875d56cf3
Katie <katiearina@gmail.com>
2017-04-30 18:29:00 -0400

    Basic Cloze functionality, though not looping.

--- assets/javascript/BasicCard.js
+++ assets/javascript/BasicCard.js
@@ -26,39 +26,6 @@ function BasicCard(front, back) {
 	};
 };
 
-// Apply printCard function to all BasicCard objects
-// BasicCard.prototype.printCard = function() {
-// 	console.log("?: " + this.front);
-// };
-
-var question1 = new BasicCard("You speak an infinite deal of nothing.", "The Merchant of Venice");
-var question2 = new BasicCard("Sweets to the sweet.", "Hamlet");
-var question3 = new BasicCard("Better a witty fool, than a foolish wit.", "Twelfth Night");
-var question4 = new BasicCard("Some Cupid kills with arrows, some with traps.", "Much Ado About Nothing");
-var question5 = new BasicCard("Educated men are so impressive!", "Romeo and Juliet");
-var question6 = new BasicCard("By the pricking of my thumbs, something wicked this way comes.", "Macbeth");
-var question7 = new BasicCard("There's meaning in thy snores.", "The Tempest");
-var question8 = new BasicCard("If I be waspish, best beware my sting.", "The Taming of the Shrew");
-
-question2.playCards();
-
-// function playCards() {
-// inquirer.prompt([
-// 	{
-// 		name: "question1",
-// 		message: question1.front
-// 	},
-
-// 	]).then(function(user) {
-// 		if (user.question1.toUpperCase() === question1.back.toUpperCase()) {
-// 			console.log("You got it right!");
-// 		}
-// 		else {
-// 			console.log("The correct answer was: " + question1.back.toUpperCase());
-// 		}
-// });
-// };
-
 
 module.exports = BasicCard;
 //---------------------------------------------------------------------------
--- assets/javascript/ClozeCard.js
+++ assets/javascript/ClozeCard.js
@@ -2,12 +2,15 @@ var ClozeCard = function(text, cloze) {
 	this.fullText = text;
 	this.cloze = cloze;
 	this.getPartial = function() {
-		var str = this.text; 
-		var res = str.replace(this.cloze, " ... ");
+		var str = this.fullText; 
+		var res = str.replace(this.cloze, "...");
 		console.log(res);
 	};
 };
 
 
+var test = new ClozeCard("I've got a lovely bunch of coconuts", "bunch");
+
+test.getPartial();
 
 module.exports = ClozeCard;
